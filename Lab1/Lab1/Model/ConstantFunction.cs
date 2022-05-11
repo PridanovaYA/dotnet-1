ed6b75abf918f52e6ec47ac61f4a6429487def38
PridanovaYA <94101848+PridanovaYA@users.noreply.github.com>
2022-05-11 10:43:28 +0400

    Реализована структура классов

---
+++ Lab1/Lab1/Model/ConstantFunction.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1.Model
+{
+    internal class ConstantFunction : Function
+    {
+        private double C { get; set; }
+
+        public ConstantFunction()
+        {
+            C = 0;
+        }
+
+        public ConstantFunction(double c)
+        {
+            C = c;
+        }
+
+        public override dynamic Calculate(double value)
+        {
+            return $"y = {C}";
+        }
+
+        public override string Derivative()
+        {
+            return "y' = 0";
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is not ConstantFunction other)
+            {
+                return false;
+            }
+            return C == other.C;
+        }
+
+        public override string ToString()
+        {
+            return $"y = {C}";
+        } 
+
+        public override int GetHashCode()
+        {
+            return C.GetHashCode();
+        }
+
+
+    }
+}
+
---
+++ Lab1/Lab1/Program.cs
@@ -0,0 +1,28 @@
+﻿using Lab1.Model;
+using System;
+
+namespace Lab1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+            Function a = new ConstantFunction(2);
+            Function b = new ExponentialFunction(2);
+            Function c = new PowerFunction(2, 3);
+            Function d = new LogarithmFunction(2);
+            Console.WriteLine(a.Derivative());
+            Console.WriteLine(a.Calculate(1));
+            Console.WriteLine("______________________________________________________");
+            Console.WriteLine(b.Derivative());
+            Console.WriteLine(b.Calculate(1));
+            Console.WriteLine("______________________________________________________");
+            Console.WriteLine(c.Derivative());
+            Console.WriteLine(c.Calculate(1));
+            Console.WriteLine("______________________________________________________");
+            Console.WriteLine(d.Derivative());
+            Console.WriteLine(d.Calculate(1));
+        }
+    }
+}
